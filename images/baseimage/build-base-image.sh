2f3ce91cf427d0ce5fd4d339e22523fc00814506
Fabian Bäumer <fabian.baeumer@ruhr-uni-bochum.de>
2021-10-21 14:10:06 +0200

    fix: Use wrapped docker command

--- images/baseimage/build-base-image.sh
+++ images/baseimage/build-base-image.sh
@@ -4,7 +4,7 @@ source "../helper-functions.sh"
 
 _docker build --build-arg VERSION=3.14 -t alpine-build:3.14 .
 
-docker build --build-arg VERSION=bullseye -f Dockerfile_debian -t debian-build:bullseye .
-docker build --build-arg VERSION=stretch --build-arg LIBSSL_VERSION=1.0 -f Dockerfile_debian -t debian-build:stretch-libssl1.0 .
+_docker build --build-arg VERSION=bullseye -f Dockerfile_debian -t debian-build:bullseye .
+_docker build --build-arg VERSION=stretch --build-arg LIBSSL_VERSION=1.0 -f Dockerfile_debian -t debian-build:stretch-libssl1.0 .
 
 exit "$EXITCODE"
