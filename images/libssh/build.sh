99d511669e99096b91e1a34e57e48b4321d69db8
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2022-08-25 16:34:33 +0200

    chore(libssh): Remove obsolete files

--- images/libssh/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")" || exit 1
-source ../helper-functions.sh
-exit_on_error
-
-track_error ./libssh-0_7_0_to_0_8_9.sh
-track_error ./libssh-0_9_x.sh
-
-exit "$EXITCODE"
