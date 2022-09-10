31cc6d99b0522d8ae769847e66742945bfc7fa07
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2022-09-10 15:59:56 +0200

    style(manger): Reformat Python code using `black`

--- images/manger/kex_rsa_oracle_patch.py
+++ images/manger/kex_rsa_oracle_patch.py
@@ -38,7 +38,7 @@ MSG_KEXRSA_DONE = 32
 class _KexRSA(Kex):
     """Handler for RSA key exchange"""
 
-    _handler_names = get_symbol_names(globals(), 'MSG_KEXRSA')
+    _handler_names = get_symbol_names(globals(), "MSG_KEXRSA")
 
     def __init__(self, alg, conn, hash_alg, key_size, hash_size):
         super().__init__(alg, conn, hash_alg)
@@ -61,11 +61,14 @@ class _KexRSA(Kex):
             host_key = self._conn.get_server_host_key()
             self._host_key_data = host_key.public_data
 
-            self._trans_key = generate_private_key('ssh-rsa', self._key_size)
+            self._trans_key = generate_private_key("ssh-rsa", self._key_size)
 
             self._trans_key_data = self._trans_key.public_data
-            self.send_packet(MSG_KEXRSA_PUBKEY, String(self._host_key_data),
-                             String(self._trans_key_data))
+            self.send_packet(
+                MSG_KEXRSA_PUBKEY,
+                String(self._host_key_data),
+                String(self._trans_key_data),
+            )
 
     def _compute_hash(self):
         """Compute a hash of key information associated with the connection"""
@@ -82,7 +85,7 @@ class _KexRSA(Kex):
         """Process a KEXRSA pubkey message"""
 
         if self._conn.is_server():
-            raise ProtocolError('Unexpected KEXRSA pubkey msg')
+            raise ProtocolError("Unexpected KEXRSA pubkey msg")
 
         self._host_key_data = packet.get_string()
         self._trans_key_data = packet.get_string()
@@ -91,7 +94,7 @@ class _KexRSA(Kex):
         try:
             trans_key = decode_ssh_public_key(self._trans_key_data)
         except KeyImportError:
-            raise ProtocolError('Invalid KEXRSA pubkey msg') from None
+            raise ProtocolError("Invalid KEXRSA pubkey msg") from None
 
         self._k = randrange(self._k_limit)
         self._encrypted_k = trans_key.encrypt(MPInt(self._k), self.algorithm)
@@ -102,18 +105,20 @@ class _KexRSA(Kex):
         """Process a KEXRSA secret message"""
 
         if self._conn.is_client():
-            raise ProtocolError('Unexpected KEXRSA secret msg')
+            raise ProtocolError("Unexpected KEXRSA secret msg")
 
         self._encrypted_k = packet.get_string()
         packet.check_end()
 
         # Implement manger vulnerability part 2: Different error message on starting byte not 00
         if not vulnerability.first_byte_is_zero(self._trans_key, self._encrypted_k):
-            raise KeyExchangeFailed('Key exchange decryption failed because first byte is not 00')
+            raise KeyExchangeFailed(
+                "Key exchange decryption failed because first byte is not 00"
+            )
 
         decrypted_k = self._trans_key.decrypt(self._encrypted_k, self.algorithm)
         if not decrypted_k:
-            raise KeyExchangeFailed('Key exchange decryption failed')
+            raise KeyExchangeFailed("Key exchange decryption failed")
 
         packet = SSHPacket(decrypted_k)
         self._k = packet.get_mpint()
@@ -132,7 +137,7 @@ class _KexRSA(Kex):
         """Process a KEXRSA done message"""
 
         if self._conn.is_server():
-            raise ProtocolError('Unexpected KEXRSA done msg')
+            raise ProtocolError("Unexpected KEXRSA done msg")
 
         sig = packet.get_string()
         packet.check_end()
@@ -141,19 +146,19 @@ class _KexRSA(Kex):
 
         h = self._compute_hash()
         if not host_key.verify(h, sig):
-            raise KeyExchangeFailed('Key exchange hash mismatch')
+            raise KeyExchangeFailed("Key exchange hash mismatch")
 
         self._conn.send_newkeys(self._k, h)
 
     _packet_handlers = {
         MSG_KEXRSA_PUBKEY: _process_pubkey,
         MSG_KEXRSA_SECRET: _process_secret,
-        MSG_KEXRSA_DONE: _process_done
+        MSG_KEXRSA_DONE: _process_done,
     }
 
 
 for _name, _hash_alg, _key_size, _hash_size, _default in (
-        (b'rsa2048-sha256', sha256, 2048, 256, True),
-        (b'rsa1024-sha1', sha1, 1024, 160, False)):
-    register_kex_alg(_name, _KexRSA, _hash_alg,
-                     (_key_size, _hash_size), _default)
+    (b"rsa2048-sha256", sha256, 2048, 256, True),
+    (b"rsa1024-sha1", sha1, 1024, 160, False),
+):
+    register_kex_alg(_name, _KexRSA, _hash_alg, (_key_size, _hash_size), _default)
