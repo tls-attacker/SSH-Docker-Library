06dea43e781ca584c7f9f7f5f43ba3e95dcac41a
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2022-09-13 12:22:30 +0200

    Generate `compose.yml` automatically (#27)
    * refactor(docker): Split compose config into separate files
    
    * chore(docker): Add compose files to .dockerignore
    
    Changes in the compose file don't necessarily invalidate the build
    context.
    
    * chore(tools): Add tool to generate docker compose file using templates
    
    * ci(pre-commit): Generate global `compose.yml` automatically
    
    * chore(tools): Add `requirements.txt`
    
    This also allows the CI to install the needed dependencies.

---
+++ tools/generate_compose_config.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+import sys
+import pathlib
+import yaml
+import jinja2
+
+
+def main(argv=None):
+    repository_root_path = pathlib.Path(__file__).parent.parent
+
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "file",
+        type=pathlib.Path,
+        nargs="*",
+        default=sorted(
+            repository_root_path.glob("images/*/compose.yml"),
+            # Ensure that image names without a `-` are always sorted *after*
+            # those that include a `-` (e.g. `openssh-7.x` comes before
+            # `openssh`).
+            key=lambda p: tuple(x or "~" for x in p.parent.stem.partition("-")),
+        ),
+    )
+    parser.add_argument(
+        "-t",
+        "--template-file",
+        type=pathlib.Path,
+        default=repository_root_path.joinpath("compose.yml.tpl"),
+    )
+    parser.add_argument(
+        "-o", "--output-file", type=argparse.FileType("w"), default=sys.stdout
+    )
+    args = parser.parse_args(argv)
+
+    files = {}
+    for path in args.file:
+        logger.debug("Parsed file: %s", path)
+        with path.open("r") as fp:
+            files[path.relative_to(repository_root_path)] = yaml.safe_load(fp)
+
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(
+            # Adding `/` is necessary to allow arbitrary absolute paths.
+            searchpath=[".", "/"],
+            followlinks=True,
+        ),
+        autoescape=False,
+    )
+    logger.debug("Template file: %s", args.template_file)
+    template = env.get_template(str(args.template_file))
+    args.output_file.write(
+        template.render(
+            files=files,
+            # Pass some builtin functions to make template development easier.
+            enumerate=enumerate,
+            sorted=sorted,
+        ),
+    )
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
